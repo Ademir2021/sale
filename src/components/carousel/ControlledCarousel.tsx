67836bfe5ab61e020ce94cf3c50ea85d0997f943
Ademir2021 <centroserra@gmail.com>
2025-07-24 15:26:11 -0300

    up code

--- src/components/carousel/ControlledCarousel.tsx
+++ src/components/carousel/ControlledCarousel.tsx
@@ -5,17 +5,14 @@ import './css/styles.css'
 
 function ControlledCarousel() {
   const [index, setIndex] = useState(0);
-
   const handleSelect = (selectedIndex: SetStateAction<number>) => {
     setIndex(selectedIndex);
   };
-
-
-  return (
+  return <>
         <Carousel
-          prevIcon=''
-          nextIcon=''
-          variant='' //dark
+          prevIcon='<'
+          nextIcon='>'
+          variant='|' //dark
           activeIndex={index}
           onSelect={handleSelect}>
           <Carousel.Item interval={800}>
@@ -31,7 +28,7 @@ function ControlledCarousel() {
           <Carousel.Item interval={800}>
       <a href='contact'>
             <div id="carousel-item2">
-              <h5>Como prefere receber a sua COMPRA</h5>
+              <h2>Contamos com todos os meios de entrega</h2>
               <p>Retire na loja ou despachamos até sua localidade</p>
               <button className='btn btn-primary' onClick={() => { window.location.replace('contact') }}>Fale conosco</button>
               </div>
@@ -39,10 +36,10 @@ function ControlledCarousel() {
           </Carousel.Item>
 
           <Carousel.Item interval={800}>
-            <a href='register'>
+            <a href='login'>
             <div id='carousel-item3'>
               <h2><>Acesso rápido e seguro</></h2>
-              <p>WEB-token, confiança durante as suas compras</p>
+              <p>Acesso com Token. Segurança durante as suas compras</p>
               <button className='btn btn-primary' onClick={() => { window.location.replace('register') }}>Criar conta</button>
             </div>
             </a>
@@ -51,7 +48,7 @@ function ControlledCarousel() {
           {/* <Carousel.Caption>
           </Carousel.Caption> */}
         </Carousel>
-  )
+  </>
 }
 
 export default ControlledCarousel
