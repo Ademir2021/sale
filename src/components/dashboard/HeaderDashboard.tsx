d46e756ff9f3aa465d544b694e5ba8ca5a3628c9
Ademir2021 <centroserra@gmail.com>
2025-08-06 17:32:05 -0300

    up code

--- src/components/dashboard/HeaderDashboard.tsx
+++ src/components/dashboard/HeaderDashboard.tsx
@@ -27,7 +27,7 @@ const HeaderDashboard: React.FC<Props> = (props: Props) => {
             <p>{props.privilege == 2 ? 'Adminstrador' : 'Cliente'}</p>
             <p><b>Olá, </b>{props.name}</p>
             <p>{props.username}</p>
-            <label>{<Icon.DotsThreeOutlineVertical size={18} color='blue' />}Gerenciar suas compras nunca foi tão fácil</label>
+            <label>{<Icon.DotsThreeOutlineVertical size={18} color='blue' />}Gerenciar Loja</label>
 
             <a href='form_person' className='p-3' >{<Icon.FolderUser size={32} />} Dados do cliente</a>
             <a href='contas_receber' className='p-3' >{<Icon.ChartLineUp size={32} />} Financeiro</a>
