baa63bed0aa53a59c5d38394ec2cd51c38c08be2
Ademir2021 <centroserra@gmail.com>
2025-10-07 10:00:50 -0300

    up code

--- src/components/whats/Whats.tsx
+++ src/components/whats/Whats.tsx
@@ -2,11 +2,12 @@ import { FloatingWhatsApp } from 'react-floating-whatsapp'
 
 export function Whats() {
   // const avatar: any = <img ref="ssl_cert"/>
+  const phone_whats:any = process.env.REACT_APP_PHONE_WHATS
   return (
     <>
       <FloatingWhatsApp
-      phoneNumber={'+5544988521033'}
-      accountName={'+5544988521033'}
+      phoneNumber={phone_whats}
+      accountName={phone_whats}
       statusMessage={'Em breve retornamos sua mensagem, obrigado.'}
       chatMessage={'Olá! 🤝 \nComo podemos ajudar?'}
       placeholder={'Digite aqui sua mensagem ...'}
