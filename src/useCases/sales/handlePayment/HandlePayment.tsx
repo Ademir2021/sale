fa6d17a348453e512a090491034bd21aca9fc011
Ademir2021 <centroserra@gmail.com>
2025-11-25 13:40:16 -0300

    up code

--- src/useCases/sales/handlePayment/HandlePayment.tsx
+++ src/useCases/sales/handlePayment/HandlePayment.tsx
@@ -46,41 +46,24 @@ class HandlePayment {
             let valParc = pay / installments
             for (let i = 1; installments >= i; i++) {
                 let contaReceber: TContaAreceber = {
-                    id_conta: 0,
-                    fk_filial: 0,
-                    tipo: "",
-                    fk_venda: 0,
-                    fk_user: 0,
-                    parcela: "",
-                    valor: 0,
-                    multa: 0,
-                    juros: 0,
-                    desconto: 0,
-                    emissao: null,
-                    vencimento: null,
-                    saldo: 0,
-                    pagamento: null,
-                    recebimento: null,
-                    observacao: "",
-                    fk_pagador: 0
+                    id_conta: i,
+                    fk_filial : sale.filial,
+                    tipo : cred,
+                    fk_venda : 0,
+                    fk_user : sale.user.user_id,
+                    parcela : i + '/' + installments,
+                    valor : parseFloat(valParc.toFixed(3)),
+                    multa : 0,
+                    juros : 0,
+                    desconto : 0,
+                    emissao : new Date().toISOString(),
+                    vencimento : this.setPrazo(i).toISOString(),
+                    saldo : 0,
+                    pagamento : null,
+                    recebimento : 0,
+                    observacao : holder_id,
+                    fk_pagador : sale.person.fk_name_pers
                 };
-                contaReceber.id_conta = i
-                contaReceber.fk_filial = sale.filial
-                contaReceber.tipo = cred
-                contaReceber.fk_venda = 0
-                contaReceber.fk_user = sale.user.user_id
-                contaReceber.parcela = i + '/' + installments
-                contaReceber.valor = parseFloat(valParc.toFixed(3))
-                contaReceber.multa = 0
-                contaReceber.juros = 0
-                contaReceber.desconto = 0
-                contaReceber.emissao = new Date().toISOString()
-                contaReceber.vencimento = this.setPrazo(i).toISOString()
-                contaReceber.saldo = 0
-                contaReceber.pagamento = null
-                contaReceber.recebimento = 0
-                contaReceber.observacao = holder_id
-                contaReceber.fk_pagador = sale.person.fk_name_pers
                 sale.duplicatas.push(contaReceber)
             }
         }
