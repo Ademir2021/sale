d913da78118b4e03229bb48a84a5632abb667e69
Ademir2021 <centroserra@gmail.com>
2025-11-28 16:35:02 -0300

    up code

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -12,10 +12,10 @@ export function Header(props: Props) {
     return <>
      <div className='header'>
         <a href='pe' className='header-car' >
-            <div className='header-counter'>{props.counter}</div>
-            {<Icon.ShoppingCart size={32} color='white' alt='Carrinho' />}</a>
-        {props.subtotal && <div className='header-sub-total'>
-            {props.subtotal}</div>}
+        <b className='header-counter'>{props.counter}</b>
+            {<Icon.ShoppingCart size={28} color='white' alt='Carrinho' />}</a>
+        {props.subtotal && <b className='header-sub-total'>
+            {props.subtotal}</b>}
         {<a href={"contact"} className='header-contact'>
             {<Icon.PhoneCall size={26} color='white' alt='Contato' />}</a>}
         {<a href='contact' className='header-frete'>
