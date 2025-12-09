518ae8686f0df509576540fb3fe0f9898b7f0912
Ademir2021 <centroserra@gmail.com>
2025-12-09 09:05:18 -0300

    up code

--- src/components/utils/errors/Error404.tsx
+++ src/components/utils/errors/Error404.tsx
@@ -1,12 +1,12 @@
-// import { useEffect } from "react";
+import { useEffect } from "react";
 import { CloseX } from '../closeX/CloseX';
 import './styles/styles.css'
 export function Error404() {
-    // useEffect(() => {
-    //     const uri = window.location.pathname
-    //     if (uri === "/store")
-    //         return window.location.replace('/')
-    // }, [])
+    useEffect(() => {
+        const uri = window.location.pathname
+        if (uri === "/store")
+            return window.location.replace('/')
+    }, [])
     return <>
         <div className="error-container">
             <CloseX link={'/'} />
